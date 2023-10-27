2f6abd2a6e14f31356f0abf2934b117a0e813cef
daehyeon112 <146954798+daehyeon112@users.noreply.github.com>
2023-10-27 23:55:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-    
-    if($.localstorage.isset('memo')){
-        $('.tarea').val($.localstorage.get('memo'));
+    if(localStorage.getItem('memo')){
+        $('.tarea').val(localStorage.getItem('memo'));
     }
     $('.btn_area').click(function(){
-        $.localstorage.set('memo',$('.tarea').val());
+        localStorage.setItem('memo', $('.tarea').val());
         alert('저장 되었습니다');
     });
 });
